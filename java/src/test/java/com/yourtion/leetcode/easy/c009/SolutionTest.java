92187e8c8a51c994c1f119947fff0e2cabe2aa74
YourtionGuo <yourtion@gmail.com>
2020-01-02 00:10:55 +0800

    Solution[Java]: 009-回文数

---
+++ java/src/test/java/com/yourtion/leetcode/easy/c009/SolutionTest.java
@@ -0,0 +1,27 @@
+package com.yourtion.leetcode.easy.c009;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    void runTest(int source, boolean res) {
+        System.out.printf("runTest: %d, res: %b \n", source, res);
+        assertEquals(res, new Solution().isPalindrome(source));
+    }
+
+    @Test
+    void isPalindromeBase() {
+        runTest(121, true);
+        runTest(-121, false);
+        runTest(10, false);
+        runTest(12321, true);
+    }
+
+    @Test
+    void isPalindrome1() {
+        runTest(0, true);
+        runTest(1, true);
+    }
+}
