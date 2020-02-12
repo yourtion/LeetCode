f477d4bcbbc669b4fb8502219338ac773d9826df
YourtionGuo <yourtion@gmail.com>
2020-02-12 18:55:56 +0800

    Solution[Java]: 205. 同构字符串

---
+++ java/src/test/java/com/yourtion/leetcode/easy/c0205/SolutionTest.java
@@ -0,0 +1,18 @@
+package com.yourtion.leetcode.easy.c0205;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    void runTest(String s1, String s2, boolean res) {
+        System.out.printf("runTest: %s %s , res: %b \n", s1, s2, res);
+        Assertions.assertEquals(res, new Solution().isIsomorphic(s1, s2));
+    }
+
+    @Test
+    void isIsomorphic() {
+        runTest("egg", "add", true);
+        runTest("foo", "bar", false);
+        runTest("paper", "title", true);
+    }
+}
