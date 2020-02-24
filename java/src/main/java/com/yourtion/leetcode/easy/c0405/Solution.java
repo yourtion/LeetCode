542ed62c62a4f5eb0e30414d3f27413b756e7f62
YourtionGuo <yourtion@gmail.com>
2020-02-24 21:20:31 +0800

    Solution[Java]: 405. 数字转换为十六进制数

---
+++ java/src/main/java/com/yourtion/leetcode/easy/c0405/Solution.java
@@ -0,0 +1,21 @@
+package com.yourtion.leetcode.easy.c0405;
+
+/**
+ * 405. 数字转换为十六进制数
+ *
+ * @author Yourtion
+ * @link https://leetcode-cn.com/problems/convert-a-number-to-hexadecimal/
+ */
+public class Solution {
+
+    public static final String[] map = "0123456789abcdef".split("");
+
+    public String toHex(int num) {
+        StringBuilder sb = new StringBuilder();
+        while (num != 0) {
+            sb.insert(0, map[(num & 15)]);
+            num >>>= 4;
+        }
+        return sb.toString();
+    }
+}
