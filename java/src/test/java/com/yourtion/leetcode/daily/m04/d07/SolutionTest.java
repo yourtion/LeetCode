31c390c6f090b03f934aa321e3d10191eabfcbb1
YourtionGuo <yourtion@gmail.com>
2020-04-07 14:34:58 +0800

    Solution[Java]: 每日一题20200407

---
+++ java/src/test/java/com/yourtion/leetcode/daily/m04/d07/SolutionTest.java
@@ -0,0 +1,22 @@
+package com.yourtion.leetcode.daily.m04.d07;
+
+import com.yourtion.leetcode.TestUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    void runTest(String source, String res) {
+        System.out.printf("runTest: %s , res: %s \n", source, res);
+        int[][] ss = TestUtils.stringToInt2dArray(source);
+        new Solution().rotate(ss);
+        Assertions.assertEquals(res, TestUtils.int2dArrayToString(ss));
+    }
+
+    @Test
+    void rotate() {
+        runTest("[[1,2,3],[4,5,6],[7,8,9]]", "[[7, 4, 1],[8, 5, 2],[9, 6, 3]]");
+        runTest("[[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]",
+                "[[15, 13, 2, 5],[14, 3, 4, 1],[12, 6, 8, 9],[16, 7, 10, 11]]");
+    }
+}
