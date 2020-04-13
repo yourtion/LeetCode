3ca23cd06d6f73f4692954bede6571c98b2cc9ce
YourtionGuo <yourtion@gmail.com>
2020-04-13 15:52:29 +0800

    Solution[Java]: 985. 查询后的偶数和

--- java/src/main/java/com/yourtion/leetcode/easy/c0985/Solution.java
+++ java/src/main/java/com/yourtion/leetcode/easy/c0985/Solution.java
@@ -8,6 +8,27 @@ package com.yourtion.leetcode.easy.c0985;
  */
 public class Solution {
     public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
-        return new int[]{};
+        int[] ret = new int[queries.length];
+        int sum = 0;
+        for (int t : A) {
+            if (t % 2 == 0) {
+                sum += t;
+            }
+        }
+        for (int i = 0; i < queries.length; i++) {
+            int[] q = queries[i];
+            int a = q[0];
+            int b = q[1];
+            int tmp = A[b];
+            boolean change1 = A[b] % 2 == 0;
+            A[b] += a;
+            boolean change2 = A[b] % 2 == 0;
+            if (change1 || change2) {
+                sum = change1 ? sum - tmp : sum;
+                sum = change2 ? sum + A[b] : sum;
+            }
+            ret[i] = sum;
+        }
+        return ret;
     }
 }
