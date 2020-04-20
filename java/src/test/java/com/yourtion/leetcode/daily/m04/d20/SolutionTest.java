1d7dce743d69820f5ed0519c355441ae31265cca
YourtionGuo <yourtion@gmail.com>
2020-04-20 17:39:18 +0800

    Solution[Java]: 每日一题20200420

---
+++ java/src/test/java/com/yourtion/leetcode/daily/m04/d20/SolutionTest.java
@@ -0,0 +1,20 @@
+package com.yourtion.leetcode.daily.m04.d20;
+
+import com.yourtion.leetcode.TestUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    void runTest(String source, int res) {
+        System.out.printf("runTest: %s , res: %d \n", source, res);
+        char[][] ss = TestUtils.stringToChar2dArray(source);
+        Assertions.assertEquals(res, new Solution().numIslands(ss));
+    }
+
+    @Test
+    void numIslands() {
+        runTest("[[\"1\",\"1\",\"1\",\"1\",\"0\"],[\"1\",\"1\",\"0\",\"1\",\"0\"],[\"1\",\"1\",\"0\",\"0\",\"0\"],[\"0\",\"0\",\"0\",\"0\",\"0\"]]", 1);
+        runTest("[[\"1\",\"1\",\"0\",\"0\",\"0\"],[\"1\",\"1\",\"0\",\"0\",\"0\"],[\"0\",\"0\",\"1\",\"0\",\"0\"],[\"0\",\"0\",\"0\",\"1\",\"1\"]]", 3);
+    }
+}
