6a829b3387b3703b2968ae05e8dfc2cca00686a8
YourtionGuo <yourtion@gmail.com>
2020-04-30 09:35:57 +0800

    Solution[Java]: 每日一题20200430

---
+++ java/src/main/java/com/yourtion/leetcode/daily/m04/d30/Solution.java
@@ -0,0 +1,24 @@
+package com.yourtion.leetcode.daily.m04.d30;
+
+/**
+ * 202. 快乐数
+ *
+ * @author Yourtion
+ * @link https://leetcode-cn.com/problems/happy-number/
+ */
+public class Solution {
+    public boolean isHappy(int n) {
+        if (n == 1 || n == 7) {
+            return true;
+        } else if (n < 10) {
+            return false;
+        }
+        int res = 0;
+        while (n > 0) {
+            int bit = n % 10;
+            res += bit * bit;
+            n = n / 10;
+        }
+        return isHappy(res);
+    }
+}
---
+++ java/src/test/java/com/yourtion/leetcode/daily/m04/d30/SolutionTest.java
@@ -0,0 +1,26 @@
+package com.yourtion.leetcode.daily.m04.d30;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    void runTest(int source, boolean res) {
+        System.out.printf("runTest: %d , res: %b \n", source, res);
+        Assertions.assertEquals(res, new Solution().isHappy(source));
+    }
+
+    @Test
+    void isHappy() {
+        runTest(19, true);
+        runTest(68, true);
+        runTest(1, true);
+        runTest(2, false);
+        runTest(3, false);
+        runTest(4, false);
+        runTest(5, false);
+        runTest(6, false);
+        runTest(7, true);
+        runTest(8, false);
+        runTest(9, false);
+    }
+}
