07179dcf9140992e1db24124a3e78a3c0533fd12
YourtionGuo <yourtion@gmail.com>
2020-05-03 19:16:43 +0800

    Solution[Java]: 1356. 根据数字二进制下 1 的数目排序

---
+++ java/src/main/java/com/yourtion/leetcode/easy/c1356/Solution.java
@@ -0,0 +1,24 @@
+package com.yourtion.leetcode.easy.c1356;
+
+import java.util.Arrays;
+
+/**
+ * 1356. 根据数字二进制下 1 的数目排序
+ *
+ * @author Yourtion
+ * @link https://leetcode-cn.com/problems/sort-integers-by-the-number-of-1-bits/
+ */
+public class Solution {
+
+    public int[] sortByBits(int[] arr) {
+        int[] map = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            map[i] = Integer.bitCount(arr[i]) * 10000000 + arr[i];
+        }
+        Arrays.sort(map);
+        for (int i = 0; i < map.length; i++) {
+            map[i] = map[i] % 10000000;
+        }
+        return map;
+    }
+}
---
+++ java/src/test/java/com/yourtion/leetcode/easy/c1356/SolutionTest.java
@@ -0,0 +1,35 @@
+package com.yourtion.leetcode.easy.c1356;
+
+import com.yourtion.leetcode.TestUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("1356. 根据数字二进制下 1 的数目排序")
+class SolutionTest {
+
+    static Stream<Arguments> testDataProvider() {
+        return Stream.of(
+                arguments("[0,1,2,3,4,5,6,7,8]", "[0, 1, 2, 4, 8, 3, 5, 6, 7]"),
+                arguments("[1024,512,256,128,64,32,16,8,4,2,1]", "[1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]"),
+                arguments("[10000,10000]", "[10000, 10000]"),
+                arguments("[2,3,5,7,11,13,17,19]", "[2, 3, 5, 17, 7, 11, 13, 19]"),
+                arguments("[10,100,1000,10000]", "[10, 100, 10000, 1000]")
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("testDataProvider")
+    void sortByBits(String source, String res) {
+        System.out.printf("runTest: %s , res: %s", source, res);
+        int[] ss = TestUtils.stringToIntegerArray(source);
+        int[] ret = new Solution().sortByBits(ss);
+        Assertions.assertEquals(res, TestUtils.integerArrayToString(ret));
+    }
+}
