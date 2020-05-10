17929e38d92b078dc16ae6e7db58b18fe71fd69a
YourtionGuo <yourtion@gmail.com>
2020-05-10 18:23:04 +0800

    Solution[Java]: 1456. 面试题 01.01. 判定字符是否唯一

---
+++ java/src/main/java/com/yourtion/leetcode/easy/c1456/Solution.java
@@ -0,0 +1,19 @@
+package com.yourtion.leetcode.easy.c1456;
+
+/**
+ * 1456. 面试题 01.01. 判定字符是否唯一
+ *
+ * @author Yourtion
+ * @link https://leetcode-cn.com/problems/is-unique-lcci/
+ */
+public class Solution {
+    public boolean isUnique(String astr) {
+        char[] arr = astr.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (astr.indexOf(arr[i], i + 1) > -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
