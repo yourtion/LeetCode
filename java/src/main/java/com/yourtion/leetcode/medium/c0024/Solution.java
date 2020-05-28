37a3fe191e6d19e311d6b32f1c9193d7807d9c3b
YourtionGuo <yourtion@gmail.com>
2020-05-28 17:49:14 +0800

    Solution[Java]: 24. 两两交换链表中的节点

---
+++ java/src/main/java/com/yourtion/leetcode/medium/c0024/Solution.java
@@ -0,0 +1,25 @@
+package com.yourtion.leetcode.medium.c0024;
+
+import com.yourtion.leetcode.utils.ListNode;
+
+/**
+ * 24. 两两交换链表中的节点
+ *
+ * @author Yourtion
+ * @link https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+ */
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode node = head;
+        while (node != null) {
+            if (node.next != null) {
+                int tmp = node.val;
+                node.val = node.next.val;
+                node.next.val = tmp;
+                node = node.next;
+            }
+            node = node.next;
+        }
+        return head;
+    }
+}
