0fd31700d470202b06db65cb18b20e55877fd629
YourtionGuo <yourtion@gmail.com>
2020-06-04 15:58:54 +0800

    Solution[Java]: 75. 颜色分类

---
+++ java/src/test/java/com/yourtion/leetcode/medium/c0075/SolutionTest.java
@@ -0,0 +1,31 @@
+package com.yourtion.leetcode.medium.c0075;
+
+import com.yourtion.leetcode.TestUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("75. 颜色分类")
+class SolutionTest {
+
+    static Stream<Arguments> testDataProvider() {
+        return Stream.of(
+                arguments("[2,0,2,1,1,0]", "[0, 0, 1, 1, 2, 2]")
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("testDataProvider")
+    void sortColors(String source, String res) {
+        System.out.printf("runTest: %s , res: %s", source, res);
+        int[] ss = TestUtils.stringToIntegerArray(source);
+        new Solution().sortColors(ss);
+        Assertions.assertEquals(res, TestUtils.integerArrayToString(ss));
+    }
+}
