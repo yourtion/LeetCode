ab779658483ffc62e0f89a80dbd090689e5c5e6f
YourtionGuo <yourtion@gmail.com>
2020-06-06 18:47:18 +0800

    Solution[Java]: 78. 子集

---
+++ java/src/test/java/com/yourtion/leetcode/medium/c0078/SolutionTest.java
@@ -0,0 +1,32 @@
+package com.yourtion.leetcode.medium.c0078;
+
+import com.yourtion.leetcode.TestUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("78. 子集")
+class SolutionTest {
+    static Stream<Arguments> testDataProvider() {
+        return Stream.of(
+                arguments("[1,2,3]", "[[],[1],[1, 2],[1, 2, 3],[1, 3],[2],[2, 3],[3]]")
+        );
+    }
+
+
+    @ParameterizedTest()
+    @MethodSource("testDataProvider")
+    void subsets(String source, String res) {
+        System.out.printf("runTest: %s , res: %s", source, res);
+        int[] ss = TestUtils.stringToIntegerArray(source);
+        List<List<Integer>> ret = new Solution().subsets(ss);
+        Assertions.assertEquals(res, TestUtils.int2dListToString(ret));
+    }
+}
