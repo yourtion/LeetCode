a4607ddcb209604aafd955ce13fa64656614e95e
YourtionGuo <yourtion@gmail.com>
2020-06-19 13:42:24 +0800

    Solution[Java]: 每日一题20200619

---
+++ java/src/test/java/com/yourtion/leetcode/daily/m06/d19/SolutionTest.java
@@ -0,0 +1,29 @@
+package com.yourtion.leetcode.daily.m06.d19;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("每日一题 - 20200618")
+class SolutionTest {
+
+    static Stream<Arguments> testDataProvider() {
+        return Stream.of(
+                arguments("A man, a plan, a canal: Panama", true),
+                arguments("race a car", false)
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("testDataProvider")
+    void isPalindrome(String source, boolean res) {
+        System.out.printf("runTest: %s , res: %b", source, res);
+        Assertions.assertEquals(res, new Solution().isPalindrome(source));
+    }
+}
