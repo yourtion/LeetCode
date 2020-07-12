6fe81d71557a09821a1cd1f4880ce657922217c0
YourtionGuo <yourtion@gmail.com>
2020-07-12 13:08:32 +0800

    Solution[Java]: 每日一题20200712

---
+++ java/src/test/java/com/yourtion/leetcode/daily/m07/d12/SolutionTest.java
@@ -0,0 +1,30 @@
+package com.yourtion.leetcode.daily.m07.d12;
+
+import com.yourtion.leetcode.TestUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("每日一题 - 20200712")
+class SolutionTest {
+
+    static Stream<Arguments> testDataProvider() {
+        return Stream.of(
+                arguments("[[-2,-3,3],[-5,-10,1],[10,30,-5]]", 7)
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("testDataProvider")
+    void calculateMinimumHP(String source, int res) {
+        System.out.printf("runTest: %s , res: %d", source, res);
+        int[][] ss = TestUtils.stringToInt2dArray(source);
+        Assertions.assertEquals(res, new Solution().calculateMinimumHP(ss));
+    }
+}
