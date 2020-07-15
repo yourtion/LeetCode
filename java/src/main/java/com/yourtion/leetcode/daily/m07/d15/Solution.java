5f5346b7568f62d190e49eca6538792e4c6c139d
YourtionGuo <yourtion@gmail.com>
2020-07-15 12:01:38 +0800

    Solution[Java]: 每日一题20200715

---
+++ java/src/main/java/com/yourtion/leetcode/daily/m07/d15/Solution.java
@@ -0,0 +1,17 @@
+package com.yourtion.leetcode.daily.m07.d15;
+
+/**
+ * 96. 不同的二叉搜索树
+ *
+ * @author Yourtion
+ * @link https://leetcode-cn.com/problems/unique-binary-search-trees/
+ */
+public class Solution {
+    public int numTrees(int n) {
+        long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int) C;
+    }
+}
---
+++ java/src/test/java/com/yourtion/leetcode/daily/m07/d15/SolutionTest.java
@@ -0,0 +1,28 @@
+package com.yourtion.leetcode.daily.m07.d15;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("每日一题 - 20200715")
+class SolutionTest {
+
+    static Stream<Arguments> testDataProvider() {
+        return Stream.of(
+                arguments(3, 5)
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("testDataProvider")
+    void numTrees(int source, int res) {
+        System.out.printf("runTest: %d , res: %d", source, res);
+        Assertions.assertEquals(res, new Solution().numTrees(source));
+    }
+}
