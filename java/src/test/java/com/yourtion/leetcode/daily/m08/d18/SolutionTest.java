ca1808e6c03e6b9b52145f4a0bdff68e683b5f55
YourtionGuo <yourtion@gmail.com>
2020-08-18 11:27:04 +0800

    Solution[Java]: 每日一题20200818

---
+++ java/src/test/java/com/yourtion/leetcode/daily/m08/d18/SolutionTest.java
@@ -0,0 +1,33 @@
+package com.yourtion.leetcode.daily.m08.d18;
+
+import com.yourtion.leetcode.TestUtils;
+import com.yourtion.leetcode.utils.ListNode;
+import com.yourtion.leetcode.utils.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("每日一题 - 20200818")
+class SolutionTest {
+
+    static Stream<Arguments> testDataProvider() {
+        return Stream.of(
+                arguments("[-10, -3, 0, 5, 9]", "[0, -10, 5, null, -3, null, 9]")
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("testDataProvider")
+    void sortedListToBST(String source, String res) {
+        System.out.printf("runTest: %s , res: %s", source, res);
+        ListNode head = TestUtils.stringToListNode(source);
+        TreeNode ret = new Solution().sortedListToBST(head);
+        Assertions.assertEquals(res, TestUtils.treeNodeToString(ret));
+    }
+}
