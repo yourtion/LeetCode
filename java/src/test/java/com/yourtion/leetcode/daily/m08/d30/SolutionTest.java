a5772be5874956c4509836a17578327f5e3e9f17
YourtionGuo <yourtion@gmail.com>
2020-08-30 10:03:33 +0800

    Solution[Java]: 每日一题20200830

---
+++ java/src/test/java/com/yourtion/leetcode/daily/m08/d30/SolutionTest.java
@@ -0,0 +1,31 @@
+package com.yourtion.leetcode.daily.m08.d30;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("每日一题 - 20200830")
+class SolutionTest {
+
+    static Stream<Arguments> testDataProvider() {
+        return Stream.of(
+                arguments("the sky is blue", "blue is sky the"),
+                arguments("  hello world!  ", "world! hello"),
+                arguments("a good   example", "example good a"),
+                arguments("", "")
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("testDataProvider")
+    void reverseWords(String source, String res) {
+        System.out.printf("runTest: %s , res: %s ", source, res);
+        Assertions.assertEquals(res, new Solution().reverseWords(source));
+    }
+}
