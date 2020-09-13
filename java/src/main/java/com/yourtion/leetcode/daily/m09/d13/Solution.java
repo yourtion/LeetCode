6a8662fcbbe69fbf6617a71ee04673265cf72a1f
YourtionGuo <yourtion@gmail.com>
2020-09-13 16:57:19 +0800

    Solution[Java]: 每日一题20200913

---
+++ java/src/main/java/com/yourtion/leetcode/daily/m09/d13/Solution.java
@@ -0,0 +1,54 @@
+package com.yourtion.leetcode.daily.m09.d13;
+
+/**
+ * 79. 单词搜索
+ *
+ * @author Yourtion
+ * @link https://leetcode-cn.com/problems/word-search/
+ */
+public class Solution {
+    private static final int[][] DIRECTION = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    private boolean[][] marked;
+    private char[][] board;
+    private char[] chars;
+    private int m;
+    private int n;
+
+    private boolean inArea(int x, int y) {
+        return x >= 0 && x < m && y >= 0 && y < n;
+    }
+
+    private boolean search(int i, int j, int start) {
+        if (start == chars.length - 1) {
+            return board[i][j] == chars[start];
+        }
+        if (board[i][j] == chars[start]) {
+            marked[i][j] = true;
+            for (int[] dis : DIRECTION) {
+                int newX = i + dis[0];
+                int newY = j + dis[1];
+                if (inArea(newX, newY) && !marked[newX][newY] && search(newX, newY, start + 1)) {
+                    return true;
+                }
+            }
+            marked[i][j] = false;
+        }
+        return false;
+    }
+
+    public boolean exist(char[][] board, String word) {
+        this.board = board;
+        m = board.length;
+        n = board[0].length;
+        marked = new boolean[m][n];
+        this.chars = word.toCharArray();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (search(i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ java/src/test/java/com/yourtion/leetcode/daily/m09/d13/SolutionTest.java
@@ -0,0 +1,36 @@
+package com.yourtion.leetcode.daily.m09.d13;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("每日一题 - 20200913")
+class SolutionTest {
+
+    static Stream<Arguments> testDataProvider() {
+        char[][] board = new char[][]{
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'C', 'S'},
+                {'A', 'D', 'E', 'E'}
+        };
+        return Stream.of(
+                arguments(board, "ABCCED", true),
+                arguments(board, "SEE", true),
+                arguments(board, "ABCB", false)
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("testDataProvider")
+    void exist(char[][] board, String word, boolean res) {
+        System.out.printf("runTest: %s  %s , res: %b", Arrays.deepToString(board), word, res);
+        Assertions.assertEquals(res, new Solution().exist(board, word));
+    }
+}
