1eb577bf418c53440ec1cebcdf599af19d8cb255
YourtionGuo <yourtion@gmail.com>
2020-09-18 09:54:25 +0800

    Solution[Java]: 每日一题20200918

---
+++ java/src/test/java/com/yourtion/leetcode/daily/m09/d18/SolutionTest.java
@@ -0,0 +1,32 @@
+package com.yourtion.leetcode.daily.m09.d18;
+
+import com.yourtion.leetcode.TestUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("每日一题 - 20200918")
+class SolutionTest {
+
+    static Stream<Arguments> testDataProvider() {
+        return Stream.of(
+                arguments("[1,1,2]", "[[1, 1, 2],[1, 2, 1],[2, 1, 1]]")
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("testDataProvider")
+    void permuteUnique(String source, String res) {
+        System.out.printf("runTest: %s , res: %s", source, res);
+        int[] ss = TestUtils.stringToIntegerArray(source);
+        List<List<Integer>> ret = new Solution().permuteUnique(ss);
+        Assertions.assertEquals(res, TestUtils.int2dListToString(ret));
+    }
+}
