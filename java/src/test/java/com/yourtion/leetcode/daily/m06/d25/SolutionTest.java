25e4b39c7d4e1ec4eb3dcc5da8d69b8b0fde0866
YourtionGuo <yourtion@gmail.com>
2020-09-24 11:58:03 +0800

    Solution[Java]: bug 修正

--- java/src/test/java/com/yourtion/leetcode/daily/m06/d25/SolutionTest.java
+++ java/src/test/java/com/yourtion/leetcode/daily/m06/d25/SolutionTest.java
@@ -1,12 +1,13 @@
 package com.yourtion.leetcode.daily.m06.d25;
 
-import com.sun.tools.javac.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.params.provider.Arguments.arguments;
@@ -16,9 +17,9 @@ class SolutionTest {
 
     static Stream<Arguments> testDataProvider() {
         return Stream.of(
-                arguments("leetcode", List.of("leet", "code"), true),
-                arguments("applepenapple", List.of("apple", "pen"), true),
-                arguments("catsandog", List.of("cats", "dog", "sand", "and", "cat"), false)
+                arguments("leetcode", Arrays.asList("leet", "code"), true),
+                arguments("applepenapple", Arrays.asList("apple", "pen"), true),
+                arguments("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat"), false)
         );
     }
 
