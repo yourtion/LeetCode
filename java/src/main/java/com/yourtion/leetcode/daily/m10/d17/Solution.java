cb05c458b4387b4f9a41ed3c23a11b1a4dba05da
YourtionGuo <yourtion@gmail.com>
2020-10-17 09:32:22 +0800

    Solution[Java]: 每日一题20201017

---
+++ java/src/main/java/com/yourtion/leetcode/daily/m10/d17/Solution.java
@@ -0,0 +1,47 @@
+package com.yourtion.leetcode.daily.m10.d17;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 52. N皇后 II
+ *
+ * @author Yourtion
+ * @link https://leetcode-cn.com/problems/n-queens-ii/
+ */
+public class Solution {
+    public int totalNQueens(int n) {
+        Set<Integer> columns = new HashSet<>();
+        Set<Integer> diagonals1 = new HashSet<>();
+        Set<Integer> diagonals2 = new HashSet<>();
+        return backtrack(n, 0, columns, diagonals1, diagonals2);
+    }
+
+    private int backtrack(int n, int row, Set<Integer> columns, Set<Integer> diagonals1, Set<Integer> diagonals2) {
+        if (row == n) {
+            return 1;
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (columns.contains(i)) {
+                continue;
+            }
+            int diagonal1 = row - i;
+            if (diagonals1.contains(diagonal1)) {
+                continue;
+            }
+            int diagonal2 = row + i;
+            if (diagonals2.contains(diagonal2)) {
+                continue;
+            }
+            columns.add(i);
+            diagonals1.add(diagonal1);
+            diagonals2.add(diagonal2);
+            count += backtrack(n, row + 1, columns, diagonals1, diagonals2);
+            columns.remove(i);
+            diagonals1.remove(diagonal1);
+            diagonals2.remove(diagonal2);
+        }
+        return count;
+    }
+}
