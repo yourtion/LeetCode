fbb9ef6f1e5b582c9a4bd13c3c2f56f1a02b2907
YourtionGuo <yourtion@gmail.com>
2020-12-20 10:57:33 +0800

    Solution[Java]: 每日一题20201220

---
+++ java/src/main/java/com/yourtion/leetcode/daily/m12/d20/Solution.java
@@ -0,0 +1,36 @@
+package com.yourtion.leetcode.daily.m12.d20;
+
+/**
+ * 316. 去除重复字母
+ *
+ * @author Yourtion
+ * @link https://leetcode-cn.com/problems/remove-duplicate-letters/
+ */
+public class Solution {
+    public String removeDuplicateLetters(String s) {
+        boolean[] vis = new boolean[26];
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!vis[ch - 'a']) {
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        vis[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+                vis[ch - 'a'] = true;
+                sb.append(ch);
+            }
+            num[ch - 'a'] -= 1;
+        }
+        return sb.toString();
+    }
+}
